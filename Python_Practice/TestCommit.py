eaf3ed674c915f1d91820a23c997c06a90d35ef9
saku.isaket <saku.isaket@LAPTOP-S2GMU1FV>
2019-01-02 03:30:51 +0530

    Commiting code

---
+++ Python_Practice/TestCommit.py
@@ -0,0 +1,10 @@
+'''
+Created on 02-Jan-2019
+
+@author: saku.isaket
+'''
+#!/usr/bin/python
+import time;  # This is required to include time module.
+
+ticks = time.time()
+print "Number of ticks since 12:00am, January 1, 1970:", ticks
