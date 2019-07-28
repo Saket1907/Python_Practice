2bc156df51dfd1a30a435e86384b6180f3a79145
saku.isaket <saku.isaket@LAPTOP-S2GMU1FV>
2019-07-29 00:45:28 +0530

    Updated!

--- Python_Practice/TestDesktop.py
+++ Python_Practice/TestDesktop.py
@@ -5,4 +5,9 @@ Created on 28-Jul-2019
 '''
 
 #!/usr/bin/python
-print("Testing!!")
+input_variable = input("Please input the value :: ")
+if (input_variable <= 0) :
+    print("0 is provided")
+else :
+    for i in range(input_variable) :
+        print(" input times it will be looped")
